4d64346b350d12b2401de7c5b852f55b7e7e4868
Patrick <eng.patrickmonteiro@gmail.com>
2022-10-03 15:46:42 -0300

    remove toolbar electron

--- quasar-medusa-client/src-electron/electron-main.js
+++ quasar-medusa-client/src-electron/electron-main.js
@@ -19,21 +19,26 @@ function createWindow () {
    */
   mainWindow = new BrowserWindow({
     icon: path.resolve(__dirname, 'icons/icon.png'), // tray icon
-    width: 1000,
-    height: 600,
+    width: 1200,
+    height: 800,
     useContentSize: true,
     webPreferences: {
       contextIsolation: true,
       // More info: https://v2.quasar.dev/quasar-cli-vite/developing-electron-apps/electron-preload-script
       preload: path.resolve(__dirname, process.env.QUASAR_ELECTRON_PRELOAD)
-    }
+    },
+    titleBarOverlay: {
+      color: '#9675FF',
+      symbolColor: '#FEFEFE'
+    },
+    frame: false
   })
 
   mainWindow.loadURL(process.env.APP_URL)
 
   if (process.env.DEBUGGING) {
     // if on DEV or Production with debug enabled
-    mainWindow.webContents.openDevTools()
+    // mainWindow.webContents.openDevTools()
   } else {
     // we're on production; no access to devtools pls
     mainWindow.webContents.on('devtools-opened', () => {
