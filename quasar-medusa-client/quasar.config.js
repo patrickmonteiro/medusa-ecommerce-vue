266c8c02a0a48f86885b2de5cf9e83197cbf7b1d
luiquecruz <luiquecruz@gmail.com>
2022-10-04 12:34:44 -0300

    feat: add dotenv support and setup

--- quasar-medusa-client/quasar.config.js
+++ quasar-medusa-client/quasar.config.js
@@ -69,7 +69,7 @@ module.exports = configure(function (/* ctx */) {
 
       // publicPath: '/',
       // analyze: true,
-      // env: {},
+      env: require('dotenv').config().parsed,
       // rawDefine: {}
       // ignorePublicFolder: true,
       // minify: false,
